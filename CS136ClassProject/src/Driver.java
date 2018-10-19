b8952524bb6152cca6d2a5d9018c8b5003e35467
natha <natha@DESKTOP-HKJJ9RC>
2018-10-18 22:56:02 -0600

    test2

---
+++ CS136ClassProject/src/Driver.java
@@ -0,0 +1,45 @@
+
+
+
+
+//import statements
+import java.util.Scanner;
+
+
+
+public class Driver {
+
+	
+	
+	public static void main(String[] args) {
+    
+		Scanner scanner = new Scanner(System.in) ;
+		String manufacturer;
+	    String model;
+		Double price;
+		String carrier;
+	
+	
+	System.out.println("Enter Phone Manufacturer");
+    manufacturer = scanner.nextLine();
+	
+	System.out.println("Enter your model:  ");
+    model = scanner.nextLine();
+	
+    System.out.println("Enter the price paid:  ");
+    price = scanner.nextDouble();
+	
+    System.out.println("Enter your carrier:  ");
+    carrier = scanner.nextLine();
+	
+    
+	
+	MobilePhone phone = new MobilePhone(manufacturer, model, price, carrier);
+	
+	
+	
+	
+	
+}//end Main
+	
+	}//end Driver
---
+++ CS136ClassProject/src/MobilePhone.java
@@ -0,0 +1,37 @@
+//Nathaniel Snyder
+//MobilePhone
+
+public class MobilePhone {
+
+	private String manufacturer;
+	private String model;
+	private Double price;
+	private String carrier;
+	private String toString;
+	public String number;
+	
+	
+	
+	
+	public MobilePhone(String manufacturer, String model, Double price, String carrier) 
+	{
+		//super();
+		
+		
+		this.manufacturer = manufacturer;
+		this.model = model;
+		this.price = price;
+		this.carrier = carrier;
+		this.toString = toString;
+	
+	
+	
+	
+	}//end MobilePhone construct
+	
+	public String call(String number) {
+		return "You are calling:  " + number;
+	}
+	
+	
+}//end Class MobilePhone
